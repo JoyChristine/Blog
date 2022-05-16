dedaeb9ce1ac3d0063358fc8cc1983a188e2e6f7
Joy Christine <joychristin2@gmail.com>
2022-05-16 12:59:10 +0300

    initial commit

--- app/email.py
+++ app/email.py
@@ -1,12 +1,11 @@
-from flask_mail import Message
-from flask import render_template
-from . import mail
-import os
+# from flask_mail import Message
+# from flask import render_template
+# from . import mail
+# import os
 
 
-def mail_message(subject,template,to,**kwargs):
-    sender_email = os.environ['MAIL_USERNAME']
-    email = Message(subject, sender=sender_email, recipients=[to])
-    email.body= render_template(template + ".txt",**kwargs)
-    email.html = render_template(template + ".html",**kwargs)
-    mail.send(email)
+# def mail_message(subject,template,to,**kwargs):
+#     sender_email = os.environ.get('MAIL_USERNAME')
+#     email = Message(subject, sender=sender_email, recipients=[to])
+#     email.body= render_template(template + ".txt",**kwargs)
+#     mail.send(email)

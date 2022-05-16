a637fd21445ed915b3659a13b33df041215b9407
Joy Christine <joychristin2@gmail.com>
2022-05-16 03:25:44 +0300

    add class config

--- config.py
+++ config.py
@@ -7,6 +7,13 @@ class Config:
     SECRET_KEY = 'MODIFY'
     WTF_CSRF_ENABLED = False
 
+
+    MAIL_SERVER='smtp.googlemail.com'
+    MAIL_PORT= 465
+    MAIL_USE_TLS= False
+    MAIL_USE_SSL=True
+    MAIL_USERNAME= os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD= os.environ.get('MAIL_PASSWORD')
     
 class ProdConfig(Config):
     pass
